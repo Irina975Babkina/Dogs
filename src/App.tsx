30446abe5761516c39a6aae98f1379e0cb82f789
Aleksandra Lysachok <aleksandra@lysachok.dev>
2024-04-11 11:01:25 -0400

    Component MainBlock migrated from JS to TS.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import MainBlock from './components/MainBlock';
 function App() {
   return (
     <div className="container">
-      <h1>Сute cats</h1>
+      <h1>Сute dogs</h1>
       <MainBlock/> 
     </div>
   );
